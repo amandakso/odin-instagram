d227d28a403f542bff96fabbc33baacd025fbbdb
Amanda So <soamanda95@gmail.com>
2022-09-30 15:06:44 -0700

    add follow/unfollow function

--- src/components/ProfileNumbers.js
+++ src/components/ProfileNumbers.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getProfileInfo } from '../firebase/firebase.config';
+import { getProfileInfo, followAccount, unfollowAccount } from '../firebase/firebase.config';
 import "../styles/ProfileNumbers.css";
 
 const ProfileNumbers = (props) => {
@@ -7,29 +7,57 @@ const ProfileNumbers = (props) => {
     const [followers, setFollowers] = useState([]);
     const [following, setFollowing] = useState([]);
     const [followStatus, setFollowStatus] = useState("");
+    const [updateNumbers, setUpdateNumbers] = useState(true);
+    console.log(following);
 
-    
-    useEffect (() => {
-        (async () => {
-            const info = await getProfileInfo(props.uid);
+    const follow = async (account, user) => {
+        try {
+            await followAccount(account, user);
+            setFollowStatus(true);
+            setUpdateNumbers(true);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const unfollow = async (account, user) => {
+        try {
+            await unfollowAccount(account, user);
+            setFollowStatus(false);
+            setUpdateNumbers(true);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        let status = false;
+        for (let i = 0; i < followers.length; i++) {
+            console.log(followers[i]);
             console.log(props.user.uid);
-            console.log(props.uid);
-            let status = false;
-            for (let i = 0; i < info[1].info.length; i++) {
-                if (info[1].info[i] === props.user.uid) {
-                    status = true;
-                };
+            if (followers[i].follower === props.user.uid) {
+                status = true;
             };
-            if (status) {
-                setFollowStatus(true);
-            } else {
-                setFollowStatus(false);
-            }
-            setPosts(info[0].info);
-            setFollowers(info[1].info);
-            setFollowing(info[2].info);         
-        })();
-    },[props.uid, props.user]); 
+        };
+        if (status) {
+            setFollowStatus(true);
+        } else {
+            setFollowStatus(false);
+        }
+    }, [followers, props.user])
+
+    useEffect (() => {
+        if (updateNumbers) {
+            (async () => {
+                const info = await getProfileInfo(props.uid);
+                setPosts(info[0].info);
+                setFollowers(info[1].info);
+                setFollowing(info[2].info); 
+                setUpdateNumbers(false);        
+            })();
+        }
+    },[updateNumbers, props.uid, props.user]); 
+
     return (
         <div className="profile-right-container">
             <div className="numbers-container">
@@ -46,7 +74,14 @@ const ProfileNumbers = (props) => {
                     <p>following</p>
                 </div>
             </div>
-            <button className="follow">Follow</button>
+            { props.uid !== props.user.uid &&             
+                <div>
+                    { followStatus 
+                        ? <button onClick={() => unfollow(props.uid, props.user.uid)} className="follow">Unfollow</button>
+                    : <button onClick={() => follow(props.uid, props.user.uid)} className="follow">Follow</button>
+                } 
+                </div> 
+            }         
         </div>
     )
 }
